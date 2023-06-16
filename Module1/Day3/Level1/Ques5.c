57471b1772dde1172460c421b397c237138ef902
Rajdeep-Singh-Rajput <neha.pamnani@cyware.com>
2023-06-16 15:02:15 +0530

    Adding solutions

--- Module1/Day3/Level1/Ques5.c
+++ Module1/Day3/Level1/Ques5.c
@@ -1,43 +1,41 @@
 #include <stdio.h>
 
-int main() {
-    int n, i, j, number, digit;
-    int smallest, largest;
-    int valid; // Flag to check if the number is valid
-
-    printf("Enter the number of test cases: ");
-    scanf("%d", &n);
-
-    for (i = 1; i <= n; i++) {
-        printf("Enter number %d: ", i);
-        scanf("%d", &number);
-
-        smallest = 9; // Initialize smallest to maximum possible digit
-        largest = 0;  // Initialize largest to minimum possible digit
-        valid = 1;    // Initialize the validity flag to 1 (true)
-
-        // Find smallest and largest digits in the number
-        while (number != 0) {
-            digit = number % 10;
-
-            if (digit < smallest) {
-                smallest = digit;
-            }
-
-            if (digit > largest) {
-                largest = digit;
-            }
-
-            number /= 10;
+void printNumbers(int start, int end) {
+    int i;
+    if (start < end) {
+        for (i = start; i <= end; i++) {
+            printf("%d", i);
         }
-
-        if (smallest == largest && smallest != 0) {
-            printf("Not Valid\n");
-        } else {
-            printf("Smallest digit: %d\n", smallest);
-            printf("Largest digit: %d\n", largest);
+    } else {
+        for (i = start; i >= end; i--) {
+            printf("%d", i);
         }
     }
+}
+
+void printSpaces(int n) {
+    int i;
+    for (i = 1; i <= n; i++) {
+        printf(" ");
+    }
+}
+
+void printPattern(int n) {
+    int i;
+    for (i = n; i >= 1; i--) {
+        printNumbers(1, i);
+        printSpaces(2 * (n - i));
+        printNumbers(i, 1);
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printPattern(n);
 
     return 0;
 }
